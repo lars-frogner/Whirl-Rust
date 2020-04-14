42b283d910f7d306a0fc52041bc23eb70d52b047
Lars Frogner <lars_frogner@hotmail.com>
2020-04-14 23:43:57 +0200

    Adapt Euler stepper to new fluid interface

--- src/stepping/euler.rs
+++ src/stepping/euler.rs
@@ -1,16 +1,24 @@
 //! Time stepping using the Euler-Cromer scheme.
 
 use super::Stepper;
-use crate::{eos::EquationOfState, fluid::Fluid, kernel::Kernel, num::fvar};
+use crate::{
+    eos::EquationOfState,
+    fluid::{Fluid, ParticlePositions, ParticleVelocities},
+    geometry::dim::*,
+    kernel::Kernel,
+    num::fvar,
+};
 use std::iter;
 
 /// Euler-Cromer time stepping scheme.
 #[derive(Clone, Debug)]
-pub struct EulerStepper<E, K, F>
+pub struct EulerStepper<D, E, K, F>
 where
+    D: Dim,
     E: EquationOfState,
-    K: Kernel,
-    F: Fluid,
+    K: Kernel<D>,
+    F: Fluid<D>,
+    DefaultAllocator: Allocator<D>,
 {
     equation_of_state: E,
     kernel: K,
@@ -18,15 +26,21 @@ where
     time_derivatives: F::TimeDerivatives,
 }
 
-impl<E, K, F> EulerStepper<E, K, F>
+impl<D, E, K, F> EulerStepper<D, E, K, F>
 where
+    D: Dim,
     E: EquationOfState,
-    K: Kernel,
-    F: Fluid,
+    K: Kernel<D>,
+    F: Fluid<D>,
+    DefaultAllocator: Allocator<D>,
 {
     /// Creates a new Euler-Cromer stepper for the given fluid.
-    pub fn new(equation_of_state: E, kernel: K, fluid: F) -> Self {
-        let time_derivatives = fluid.compute_time_derivatives(&equation_of_state, &kernel);
+    pub fn new(
+        equation_of_state: E,
+        kernel: K,
+        fluid: F,
+        time_derivatives: F::TimeDerivatives,
+    ) -> Self {
         Self {
             equation_of_state,
             kernel,
@@ -36,22 +50,46 @@ where
     }
 }
 
-impl<E, K, F> Stepper<F> for EulerStepper<E, K, F>
+impl<D, E, K, F> Stepper<D> for EulerStepper<D, E, K, F>
 where
+    D: Dim,
     E: EquationOfState,
-    K: Kernel,
-    F: Fluid,
+    K: Kernel<D>,
+    F: Fluid<D>,
+    DefaultAllocator: Allocator<D>,
 {
-    fn fluid(&self) -> &F {
-        &self.fluid
+    fn positions(&self) -> &ParticlePositions<D> {
+        self.fluid.positions()
+    }
+
+    fn velocities(&self) -> &ParticleVelocities<D> {
+        self.fluid.velocities()
+    }
+
+    fn kernel_widths(&self) -> &[fvar] {
+        self.fluid.kernel_widths()
+    }
+
+    fn mass_densities(&self) -> &[fvar] {
+        self.fluid.mass_densities()
+    }
+
+    fn specific_energies(&self) -> &[fvar] {
+        self.fluid.specific_energies()
+    }
+
+    fn gas_pressures(&self) -> &[fvar] {
+        self.fluid.gas_pressures()
     }
 
     fn step(&mut self, time_step: fvar) {
         self.fluid
             .evolve_primary_variables(iter::once((1.0, &self.time_derivatives)), time_step);
         self.fluid.evolve_positions(1.0, iter::empty(), time_step);
-        self.fluid.update_static_properties(&self.kernel);
-        self.fluid
-            .compute_time_derivatives(&self.equation_of_state, &self.kernel);
+        self.fluid.update(
+            &self.equation_of_state,
+            &self.kernel,
+            &mut self.time_derivatives,
+        );
     }
 }
