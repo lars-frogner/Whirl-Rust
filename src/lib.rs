505d6be8969c5edfebf6efac92b7297fdf8302fd
Lars Frogner <lars_frogner@hotmail.com>
2020-04-15 00:05:41 +0200

    Add modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 //! A Rust library for smoothed particle hydrodynamics.
 
 pub mod eos;
+pub mod error;
 pub mod fluid;
 pub mod geometry;
 pub mod initial;
 pub mod kernel;
 pub mod num;
+pub mod running;
 pub mod stepping;
 pub mod units;
