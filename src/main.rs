e13316b2954d55e0fc6c2ef8287e7b1cac9cd834
Lars Frogner <lars_frogner@hotmail.com>
2020-04-15 00:07:05 +0200

    Update main function for debugging

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,14 @@
-use whirl::{
-    eos::ideal_gas::{GasParticleType, IdealGasEOS},
-    fluid::{isothermal::IsothermalFluid1D, Fluid},
-    geometry::{Bounds1D, Dimensionality},
-    initial::{
-        grid::{GridMassDistribution1D, PopulatedGrid1D},
-        rest::StaticVelocityDistribution1D,
-    },
-    kernel::gaussian::GaussianKernel,
-    stepping::{euler::EulerStepper, Stepper},
-    units::{si::SIUnits, Units},
-};
+use whirl::running::experiments;
 
 fn main() {
-    let mass_distribution = GridMassDistribution1D::new(
-        1e-3,
-        2.0,
-        vec![PopulatedGrid1D::new(10, Bounds1D::new(0.0, 1.0))],
-    );
-    let velocity_distribution = StaticVelocityDistribution1D;
-    let equation_of_state = IdealGasEOS::new::<SIUnits>(
-        Dimensionality::One,
-        GasParticleType::Monatomic,
-        14.0 * SIUnits::AMU,
-    );
-    let kernel = GaussianKernel::new(Dimensionality::One);
-    let fluid = IsothermalFluid1D::new(300.0, &kernel, &mass_distribution, &velocity_distribution);
-    let mut stepper = EulerStepper::new(equation_of_state, kernel, fluid);
+    let mut simulation = experiments::rarefaction_wave(1.0, 1.0, 2.0, 200, 1.4)
+        .unwrap()
+        .build()
+        .unwrap();
 
-    let time_step = 1e-3;
+    let time_step = 1e-4;
     for _ in 0..10 {
-        println!("{:?}", stepper.fluid().positions());
-        stepper.step(time_step);
+        println!("{:?}", simulation.positions());
+        simulation.step(time_step);
     }
 }
