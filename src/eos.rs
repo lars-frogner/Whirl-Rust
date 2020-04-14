7ab2ee68ea2f4c10e1af75145aee72d40d6ff16d
Lars Frogner <lars_frogner@hotmail.com>
2020-04-14 23:44:45 +0200

    Use specific energy in EOS interface

--- src/eos.rs
+++ src/eos.rs
@@ -1,19 +1,25 @@
 //! Equation of state.
 
-pub mod ideal_gas;
+pub mod adiabatic_ideal_gas;
 
 use crate::num::fvar;
 
 pub trait EquationOfState {
-    /// Computes the pressure for the given internal energy density.
-    fn compute_pressure(&self, energy_density: fvar) -> fvar;
+    /// Computes the pressure for the given mass density and internal specific energy.
+    fn compute_pressure(&self, mass_density: fvar, specific_energy: fvar) -> fvar;
 
-    /// Computes the temperature for the given mass density and internal energy density.
-    fn compute_temperature(&self, mass_density: fvar, energy_density: fvar) -> fvar;
+    /// Computes the temperature for the given internal specific energy.
+    fn compute_temperature(&self, specific_energy: fvar) -> fvar;
 
     /// Computes the mass density for the given pressure and temperature.
     fn compute_mass_density(&self, pressure: fvar, temperature: fvar) -> fvar;
 
-    /// Computes the internal energy density for the given mass density and temperature.
-    fn compute_energy_density(&self, mass_density: fvar, temperature: fvar) -> fvar;
+    /// Computes the internal energy density for the given mass density and specific energy.
+    fn compute_energy_density(&self, mass_density: fvar, specific_energy: fvar) -> fvar;
+
+    /// Computes the internal specific energy for the given temperature.
+    fn compute_specific_energy(&self, temperature: fvar) -> fvar;
+
+    /// Computes the squared sound speed from the given internal specific energy.
+    fn compute_squared_sound_speed(&self, specific_energy: fvar) -> fvar;
 }
